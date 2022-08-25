7eb32191c00524964777c46ff4bb98e3206d3a52
ahanaf <ahanafabdullah9@gmail.com>
2022-08-26 01:50:30 +0600

    Mobile design Done

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        spaceMono: "'Space Mono',serif",
+      },
+
+      colors: {
+        strongCyan: "hsl(172, 67%, 45%)",
+        veryDarkCyan: "hsl(183, 100%, 15%)",
+        darkGrayishCyan: "hsl(186, 14%, 43%)",
+        grayishCyan: "hsl(184, 14%, 56%)",
+        lightGrayishCyan: "hsl(185, 41%, 84%)",
+        veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
